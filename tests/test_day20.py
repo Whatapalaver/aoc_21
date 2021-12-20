1a63a5cceda13b7f9299b9535ca63041618781d2
whatapalaver <warriorwomenblog@gmail.com>
2021-12-20 21:29:49 +0000

    Day 20 solution

---
+++ tests/test_day20.py
@@ -0,0 +1,15 @@
+from aoc.day20 import solution
+
+#
+# --- Part One ---
+#
+
+def test_part1():
+    assert solution.result_part1(None) == None
+
+#
+# --- Part Two ---
+#
+
+def test_part2():
+    assert solution.result_part2(None) == None
